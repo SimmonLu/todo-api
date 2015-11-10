fbecca906f9882d8661ad4d53710404d4faa179e
Xin Lu <luxinexcs@gmail.com>
2015-11-09 23:23:56 -0500

    add post

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-var todos = [{
-	id: 1,
-	description: 'Meet mon for lunch',
-	completed: false
-}, {
-	id: 2,
-	description: 'Go to market',
-	completed: false
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+	res.send('Todo API Root');
+});
 
 // GET /todos
 app.get('/todos', function (req, res) {
@@ -33,12 +33,24 @@ app.get('/todos/:id', function (req, res) {
 	} else {
 		res.status(404).send();
 	}
-	res.send('Asking for todo with id of ' + req.params.id);
 });
 
-app.get('/', function (req, res) {
-	res.send('Todo API Root');
+//POST /todos
+app.post('/todos', function (req, res) {
+	var body = req.body;
+
+	body.id = todoNextId++;
+
+	todos.push(body);
+	
+	console.log('description: ' + body.description);
+
+	res.json(body);
+
 });
+
+
+
 
 app.listen(PORT, function () {
 	console.log('Express listening on port ' + PORT + '!');
