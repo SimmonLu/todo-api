e69978e03e96376aaa092cc0c183f12f851b0469
Xin Lu <luxinexcs@gmail.com>
2015-11-09 22:40:29 -0500

    add todos

--- server.js
+++ server.js
@@ -2,6 +2,40 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+var todos = [{
+	id: 1,
+	description: 'Meet mon for lunch',
+	completed: false
+}, {
+	id: 2,
+	description: 'Go to market',
+	completed: false
+}];
+
+// GET /todos
+app.get('/todos', function (req, res) {
+	res.json(todos);
+});
+
+// GET / /todos/:id
+app.get('/todos/:id', function (req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo;
+
+	todos.forEach(function (todo) {
+		if(todoId === todo.id) {
+			matchedTodo = todo;
+		}
+	});
+
+	if(matchedTodo) {
+		res.json(matchedTodo);
+	} else {
+		res.status(404).send();
+	}
+	res.send('Asking for todo with id of ' + req.params.id);
+});
+
 app.get('/', function (req, res) {
 	res.send('Todo API Root');
 });
