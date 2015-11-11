f9181647f75456a461a6f69beac7a2e957e4fb0e
Xin Lu <luxinexcs@gmail.com>
2015-11-11 11:23:50 -0500

    convert DELETE todos

--- server.js
+++ server.js
@@ -70,19 +70,24 @@ app.post('/todos', function(req, res) {
 //DELETE /todos/:id
 app.delete('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	console.log(todoId);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
-	});
 
-	if (!matchedTodo) {
-		res.status(404).json({
-			"error": "No todo found with that id"
-		});
-	} else {
-		todos = _.without(todos, matchedTodo);
-		res.json(matchedTodo);
-	}
+	db.todo.destroy({
+		where: {
+			id: todoId
+		}
+	}).then(function (rowsDeleted) {
+		if(rowsDeleted === 0)  {
+			res.status(404).json({
+				error: 'No todo with id'
+			});
+		} else {
+			res.status(204).send();
+		}
+
+	}, function () {
+		res.status(500).send();
+	})
+	
 });
 
 //PUT /todos/:id
