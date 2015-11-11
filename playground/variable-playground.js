0c7fca540e26e4d5ff93b09bec9fcd9128dd6cf9
Xin Lu <luxinexcs@gmail.com>
2015-11-10 21:50:50 -0500

    Add todo and POST /todos

---
+++ playground/variable-playground.js
@@ -0,0 +1,27 @@
+// var person = {
+// 	name: 'Simon',
+// 	age: 21
+// }
+
+// function updatePerson (obj) {
+// 	// obj = {
+// 	// 	name: 'Hellw',
+// 	// 	age: 25
+// 	// };
+// 	obj.age = 25;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+//Array Example
+
+var grades = [12, 90]
+
+function addGrades (gradesArr) {
+	gradesArr.push(34);
+	debugger;
+}
+
+addGrades(grades);
+console.log(grades);
