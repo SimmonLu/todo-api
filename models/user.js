0907d75a897b3d8f3bc147afd404fc39974eedc5
Xin Lu <luxinexcs@gmail.com>
2015-11-11 13:51:23 -0500

    add hooks

--- models/user.js
+++ models/user.js
@@ -15,7 +15,16 @@ module.exports = function (sequelize, DataTypes) {
 			validate: {
 				len: [7, 100]
 			}
+		}， {
+			hooks: {
+				beforeValidate: function (user, options) {
+					if (typeof user.email === 'string') {
+						user.email = user.email.toLowerCase();
+					}
+				}
+			}
 		}
+
 	});
 
 }
